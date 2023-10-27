9642e2953039b38ec1fbf147ba73dec547d7b444
Weslan Silva <wesllan90@gmail.com>
2023-10-26 21:36:54 -0300

    adicionado 3 prop private

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,17 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+
+        private string IMEI { get; set; }
+
+        private int Memoria { get; set; }
 
         public Smartphone(string numero)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+
         }
 
         public void Ligar()
